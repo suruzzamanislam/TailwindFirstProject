2e2e45f622bb2b3b2452bc278b5e6b78c16d1e62
suruzzaman <sajukaligong21@gmail.com>
2023-09-01 21:46:55 -1200

    some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens:{
+      sm:'576px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px'
+    },
+    container:{
+      center:true,
+      padding:'1rem'
+    },
     extend: {},
   },
   plugins: [],
